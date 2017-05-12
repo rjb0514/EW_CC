e4126b4873e61601fc4f5f6b60939e644fead01a
rujianbin <328989397@qq.com>
2017-05-12 08:51:59 +0800

    creatProduct

---
+++ CCDemo/CCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CCDemo
+//
+//  Created by 茹 on 2017/5/12.
+//  Copyright © 2017年 kkx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
